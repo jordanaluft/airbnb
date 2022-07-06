84295f1febbbb4acef834ae3fdf07ddfa64980d9
Jordana <jordana.sluft@gmail.com>
2022-07-06 15:50:49 +0100

    created map to fill Card

--- airbnb/src/App.tsx
+++ airbnb/src/App.tsx
@@ -8,21 +8,18 @@ import Data from './components/Data'
 function App(){
   const dataElements = Data.map(data => {
     return <Card 
-            img={data.img} 
-            star={data.star}
-            rating={data.rating}
-            reviewCount={data.reviewCount}
-            country={data.country}
-            title={data.title}
-            price={data.price}
-            />
-  })
+                key={data.id}
+                {...data}
+             />
+     })
   return(
     <div>
       <Navbar />
       <Hero />
-      {dataElements}
-      </div>
+      <section className='cards-list'>
+          {dataElements}
+      </section>
+    </div>
     )
 }
 
--- airbnb/src/components/Card.tsx
+++ airbnb/src/components/Card.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 
 function Card(props:any){
+    let badgeText
+    
+    if(props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    } else if(props.location === "online"){
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
-            <img src={props.img} className="card--image"/>   
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+
+            <img src={props.img} className="card--image"/> 
+
             <div className="stats">
                 <img src={props.star} className="card--star"/>
                 <span>{props.rating}</span>
                 <span className="card--gray">({props.reviewCount}) ° </span>
                 <span className="card--gray">{props.country}</span>
             </div>
+
             <p>{props.title}</p>
+
             <p>
                 <span className="card--price">From ${props.price}</span>
                 / person
